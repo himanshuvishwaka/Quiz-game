18f9bf8c10532f4ffb4d7f0e53c4f5155e74be41
Himanshu Vishwakarma <107816134+himanshuvishwaka@users.noreply.github.com>
2023-07-10 17:23:07 +0530

    This is the quiz  game
    For entertainment uses.

---
+++ Quiz_Game_In_Python.py
@@ -0,0 +1,49 @@
+# This is the quiz game project in the python so this is the 1st day program
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+
+answer = input("What does ROM stand for? ")
+if answer.lower() == "Read Only Memory":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 5) * 100) + "%.")
